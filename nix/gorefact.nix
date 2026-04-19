42f82625f24e3be772f236448ce95df3cb36b02f
Denis Panfilov <gh@flaticols.dev>
2026-04-19 20:00:07 +0000

    nix: update gorefact to v0.0.12

--- nix/gorefact.nix
+++ nix/gorefact.nix
@@ -2,15 +2,15 @@
 # Do not edit manually — changes will be overwritten.
 { lib, stdenvNoCC, fetchurl }:
 let
-  version = "0.0.11";
+  version = "0.0.12";
   tarballs = {
     "aarch64-darwin" = fetchurl {
       url = "https://github.com/flaticols/gorefactor/releases/download/v${version}/gorefact_Darwin_arm64.tar.gz";
-      hash = "sha256-EM7x2slKIM8Rv6giKiL000JCj4k67Qg7ajDSQj/p6FM="; # arm
+      hash = "sha256-5911/K7tReA0WOtm36n2tv4o1Q6Z++RKo0EL9dTiWrY="; # arm
     };
     "x86_64-darwin" = fetchurl {
       url = "https://github.com/flaticols/gorefactor/releases/download/v${version}/gorefact_Darwin_x86_64.tar.gz";
-      hash = "sha256-cI3K9stl/X2HLBVtXLMzT97ArOs9T8MguYq3NgfDLKM="; # x86
+      hash = "sha256-GWvkndScR5CmV7yrjATz2F30L2jCgm2VKw6gjbtczzg="; # x86
     };
   };
 in
